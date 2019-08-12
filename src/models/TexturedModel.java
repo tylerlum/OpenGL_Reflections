c720dc1b152ef77781116dc7702ca79543a9c141
Tyler Lum <tylergwlum@gmail.com>
2019-08-12 10:16:47 -0700

    Change up all code to have water

--- src/models/TexturedModel.java
+++ src/models/TexturedModel.java
@@ -3,10 +3,12 @@ package models;
 import textures.ModelTexture;
 
 public class TexturedModel {
+	
 	private RawModel rawModel;
 	private ModelTexture texture;
+
 	
-	public TexturedModel(RawModel model, ModelTexture texture) {
+	public TexturedModel(RawModel model, ModelTexture texture){
 		this.rawModel = model;
 		this.texture = texture;
 	}
@@ -18,4 +20,5 @@ public class TexturedModel {
 	public ModelTexture getTexture() {
 		return texture;
 	}
+
 }
