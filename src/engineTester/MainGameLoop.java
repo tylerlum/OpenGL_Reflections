0beff361ccdbd5b50218e44c536738878322850a
Tyler Lum <tylergwlum@gmail.com>
2019-08-11 21:36:56 -0700

    Change model vertex to vertices and indices

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -15,18 +15,18 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer();
 		
 		float[] vertices = {
-				// Left top triangle
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				
-				// Right top triangle
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0,  // V0
+				-0.5f, -0.5f, 0,  // V1
+				0.5f, -0.5f, 0,  // V2
+				0.5f, 0.5f, 0,  // V3				
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		int[] indices = {
+				0, 1, 3,   // Top left triangle (V0, V1, V3)
+				3, 1, 2    // Bottom right triangle (V3, V1, V2)
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare();
