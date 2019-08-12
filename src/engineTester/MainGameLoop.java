700be0e481eb30403c3aa78943cc2dbc7590f2d7
Tyler Lum <tylergwlum@gmail.com>
2019-08-11 23:36:11 -0700

    Show rotating cube

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
 import entities.Entity;
+import entities.Camera;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -17,38 +18,101 @@ public class MainGameLoop {
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0,  // V0
-				-0.5f, -0.5f, 0,  // V1
-				0.5f, -0.5f, 0,  // V2
-				0.5f, 0.5f, 0,  // V3				
-		};
-		
-		int[] indices = {
-				0, 1, 3,   // Top left triangle (V0, V1, V3)
-				3, 1, 2    // Bottom right triangle (V3, V1, V2)
+		Renderer renderer = new Renderer(shader);
+
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0, 0,  // V0
-				0, 1,  // V1
-				1, 1,  // V2
-				1, 0   // V3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
-		Entity entity = new Entity(texturedModel, new Vector3f(-1, 0, 0), 0, 0, 0, 1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Camera camera = new Camera();
+		
 		while(!Display.isCloseRequested()) {
-			entity.increasePosition(0.002f, 0, 0);
-            entity.increaseRotation(0, 1, 0);
+            entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
